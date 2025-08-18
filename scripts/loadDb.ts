1b7be81a7299340dc9db7ff780b15dd13b599a2a
Saanvi Tatipalli <128191346+SaanviTati@users.noreply.github.com>
2025-08-18 22:17:50 +0000

    fixing version

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -5,3 +5,15 @@ import OpenAI from "openai"
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 
 import "dotenv/config"
+
+const { ASTRA_DB_NAMESPACE, 
+    ASTRA_DB_COLLECTION, 
+    ASTRA_DB_API_ENDPOINT, 
+    ASTRA_DB_APPLICATION_TOKEN, 
+    OPENAI_API_KEY } = process.env
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY })
+
+const ragbotData = [
+    
+]
